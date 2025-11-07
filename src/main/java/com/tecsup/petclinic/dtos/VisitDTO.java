d0f1eb1371c13fb208b4c0b3c4d764c919d04393
Vasquezcito17 <vflavio806@gmail.com>
2025-11-07 10:08:33 -0500

    cambios VisitDTO-

--- src/main/java/com/tecsup/petclinic/dtos/VisitDTO.java
+++ src/main/java/com/tecsup/petclinic/dtos/VisitDTO.java
@@ -1,8 +1,42 @@
 package com.tecsup.petclinic.dtos;
 
 public class VisitDTO {
-    private String VisitDate;
+
+    private String visitDate;
     private Integer id;
     private String description;
-    private  Integer petId;
+    private Integer petId;
+
+    // Getters y setters
+    public String getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(String visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPetId() {
+        return petId;
+    }
+
+    public void setPetId(Integer petId) {
+        this.petId = petId;
+    }
 }
